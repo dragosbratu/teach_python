3f43d322f25cf5669f50415049677cee33cf8981
Dragoș Bratu <bratu.dragos92@gmail.com>
2023-07-24 19:31:27 +0300

    Create h1.py

---
+++ 1/homework/h1.py
@@ -0,0 +1,38 @@
+# Homework 1. I want 3 quatro sagione pizzas, I'm hungry! :)
+# Date 24.07.2023
+# Dragoș Bratu
+# 
+# Ioana is the ownwer of the PyPizza. She wants to do a small application to sell pizza
+# In the begining of the program a print a nice welcome message for PyPizza 
+# e.g. :
+#     _    _      _                            _         ______     ______ _                                  
+#    | |  | |    | |                          | |        | ___ \    | ___ (_)                                 
+#    | |  | | ___| | ___ ___  _ __ ___   ___  | |_ ___   | |_/ /   _| |_/ /_ __________ _                     
+#    | |/\| |/ _ \ |/ __/ _ \| '_ ` _ \ / _ \ | __/ _ \  |  __/ | | |  __/| |_  /_  / _` |                    
+#    \  /\  /  __/ | (_| (_) | | | | | |  __/ | || (_) | | |  | |_| | |   | |/ / / / (_| |                    
+#     \/  \/ \___|_|\___\___/|_| |_| |_|\___|  \__\___/  \_|   \__, \_|   |_/___/___\__,_|                    
+#                                                               __/ |                                         
+#                                                              |___/                                          
+#     _____                              _____                                                                
+#    |  _  |                         _  |_   _|                                                               
+#    | | | |_      ___ __   ___ _ __(_)   | |  ___   __ _ _ __   __ _                                         
+#    | | | \ \ /\ / / '_ \ / _ \ '__|     | | / _ \ / _` | '_ \ / _` |                                        
+#    \ \_/ /\ V  V /| | | |  __/ |   _   _| || (_) | (_| | | | | (_| |                                        
+#     \___/  \_/\_/ |_| |_|\___|_|  (_)  \___/\___/ \__,_|_| |_|\__,_|                                        
+#                                                                                                             
+#                                                                                                             
+#    ______ _                             _                                                     _             
+#    | ___ \ |                           | |                                                   | |          _ 
+#    | |_/ / | ___  __ _ ___  ___   _ __ | | __ _  ___ ___   _   _  ___  _   _ _ __    ___   __| | ___ _ __(_)
+#    |  __/| |/ _ \/ _` / __|/ _ \ | '_ \| |/ _` |/ __/ _ \ | | | |/ _ \| | | | '__|  / _ \ / _` |/ _ \ '__|  
+#    | |   | |  __/ (_| \__ \  __/ | |_) | | (_| | (_|  __/ | |_| | (_) | |_| | |    | (_) | (_| |  __/ |   _ 
+#    \_|   |_|\___|\__,_|___/\___| | .__/|_|\__,_|\___\___|  \__, |\___/ \__,_|_|     \___/ \__,_|\___|_|  (_)
+#                                  | |                        __/ |                                           
+#                                  |_|                       |___/                                            
+# use int for how many pizzas
+# use float for quantity, price
+# use string to store the name of the Client and the mobile number in +4072000000 format.
+# use boolean if you want to give a tip to the pizza guy or not
+# using the input function enter the grams of each ingredient and at the end compute the weight of the pizza. The dough is always a CONSTANT and weighs 300.0 grams
+
+# tips: the input function is described here https://www.w3schools.com/python/ref_func_input.asp
